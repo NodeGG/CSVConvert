5074d219fd983956429466688d43297600a3dbb6
Warren Gates <warrenckhan@gmail.com>
2020-04-03 13:48:25 -0400

    Update index.js

--- index.js
+++ index.js
@@ -30,54 +30,6 @@ const main = require('./src/main');
 //Server Config
 const hostname = 'localhost';
 const port = 8000 || 8000 + Math.floor((Math.random() * 100));
-///
-
-// // Keep a global reference of the window object, if you don't, the window will
-// // be closed automatically when the JavaScript object is garbage collected.
-// let win
-//
-// function createWindow() {
-// 	// Create the browser window.
-// 	win = new BrowserWindow({
-// 		width: 800,
-// 		height: 600
-// 	})
-//
-// 	// and load the index.html of the app.
-// 	win.loadFile(__dirname + '/public/index.html')
-//
-// 	// Open the DevTools.
-// 	// win.webContents.openDevTools()
-//
-// 	// Emitted when the window is closed.
-// 	win.on('closed', () => {
-// 		// Dereference the window object, usually you would store windows
-// 		// in an array if your app supports multi windows, this is the time
-// 		// when you should delete the corresponding element.
-// 		win = null
-// 	})
-// }
-// // initialization and is ready to create browser windows.
-// // Some APIs can only be used after this event occurs.
-// app.on('ready', createWindow)
-//
-// // Quit when all windows are closed.
-// app.on('window-all-closed', () => {
-// 	// On macOS it is common for applications and their menu bar
-// 	// to stay active until the user quits explicitly with Cmd + Q
-// 	if (process.platform !== 'darwin') {
-// 		app.quit()
-// 	}
-// })
-//
-// app.on('activate', () => {
-// 	// On macOS it's common to re-create a window in the app when the
-// 	// dock icon is clicked and there are no other windows open.
-// 	if (win === null) {
-// 		createWindow()
-// 	}
-// })
-
 
 //Gets HTML File
 app.get('/', (req, res) => {
